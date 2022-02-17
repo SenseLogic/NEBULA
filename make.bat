b0db049ef8b01b5a9b6521048a96ec42a8e93186
senselogic <dev.senselogic@gmail.com>
2022-02-17 21:06:21 +0000

    % make

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-dmd -O -ofnebula.exe nebula.d PCF/base.d PCF/buffer.d PCF/cell.d PCF/cell_position_vector.d PCF/cloud.d PCF/component.d PCF/compression.d PCF/image.d PCF/property.d PCF/scalar.d PCF/scan.d PCF/stream.d PCF/vector_3.d PCF/vector_4.d
+dmd -m64 -ofnebula.exe nebula.d PCF/base.d PCF/buffer.d PCF/cell.d PCF/cell_position_vector.d PCF/cloud.d PCF/component.d PCF/compression.d PCF/image.d PCF/property.d PCF/scalar.d PCF/scan.d PCF/stream.d PCF/vector_3.d PCF/vector_4.d
 del /q *.obj
 del /q *.pdb
 pause
